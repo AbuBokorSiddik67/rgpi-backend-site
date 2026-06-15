9bd20788637981d19e9d244bb545274974f17989
AbuBokorSiddik67 <abubokorsiddik1238@gmail.com>
2026-06-15 19:02:51 +0600

    check file structure

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import app from './app.js';
-import dotenv from 'dotenv';
-import logger from './utils/logger.js';
+import app from "./app.js";
+import dotenv from "dotenv";
+import logger from "./lib/logger.js";
 
 dotenv.config();
 
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   logger.info(`[server]: Server is running at http://localhost:${PORT}`);
-  logger.info(`[swagger]: API Docs available at http://localhost:${PORT}/api-docs`);
+  logger.info(
+    `[swagger]: API Docs available at http://localhost:${PORT}/api-docs`,
+  );
 });
-
